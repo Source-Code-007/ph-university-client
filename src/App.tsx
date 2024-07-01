e875b06f84890793597cf9b8f91810e8fda0886c
Source-Code-007 <utsho926@gmail.com>
2024-07-01 10:31:16 +0600

    react router initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import Child from "./Child";
-import { useAppSelector } from "./redux/hook";
 
 function App() {
-  const { count } = useAppSelector((state) => state.counter);
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className="p-4 rounded border border-indigo-500 text-center">
-        <p className="font-bold">{count} from parent</p>
-
-        <Child />
-      </div>
+      Ph home
     </div>
   );
 }
